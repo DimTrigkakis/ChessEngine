54696b34029de376f56b76cf1d5d2b01bd307609
DimTrigkakis <trigkakd@oregonstate.edu>
2017-02-23 03:17:16 -0800

    "First visual imagery"

--- core.py
+++ core.py
@@ -1,43 +1,60 @@
 import chess
 import chess.pgn
 import PIL.Image
-import TkImage
+import ImageTk
 import Tkinter
-
+import random
 class Core():
 
     myCanvas = None
     w,h = 600,600
     border = 5
+    border_width = 20
+    pieces = [] # images for pieces
+    piece_image_labels = ["b","k","n","p","q","r"]
+    piece_names = ["b","k","n","p","q","r","B","K","N","P","Q","R"]
+
+    coord = [2.5*border,2.5*border]
+    size = (600.0-38)/8
+
+    def canvas_coordinates(self,i,j):
+        return i+self.coord[0]+self.size/2+(self.size-1)*i,j+self.coord[1]+self.size/2+(self.size-1)*j
 
     def draw_board(self,board):
-        border_width = 20
-        self.myCanvas.create_rectangle(border_width-2.5*self.border, border_width-2.5*self.border, 600-border_width, 600-border_width, fill="black")
 
-        coord = [2.5*self.border,2.5*self.border]
-        size = (600.0-38)/8
-        color1 ="#ba8146"
-        color2 ="#eec59b"
-        for i in range(8):
-            for j in range(8):
-                    color = color1
-                    if (i+j) % 2 == 0:
-                        color = color2
-                    self.myCanvas.create_rectangle(coord[0]+size*i,coord[1]+size*j,coord[0]+size*(i+1),coord[1]+size*(j+1),fill=color)
+        self.myCanvas.create_rectangle(self.border_width-2.5*self.border, self.border_width-2.5*self.border, 600-self.border_width, 600-self.border_width, fill="black")
 
+        colors = ["#ba8146","#eec59b"]
+        for g in range(64):
+            self.myCanvas.create_rectangle(self.coord[0]+self.size*(g%8),self.coord[1]+self.size*(g/8),self.coord[0]+self.size*(g%8+1),self.coord[1]+self.size*(g/8+1),fill=colors[(g%8+g/8) % 2])
 
-        img = PIL.Image.open("./img/blackr.png")
-        tk_img = ImageTk.PhotoImage(img)
-        self.myCanvas.create_image(250, 250,image=tk_img)
-
+        position = board.board_fen()
+        k = 0
+        for i,p in enumerate(position):
+            if p != "/" and not p.isdigit():
+                self.myCanvas.create_image(*self.canvas_coordinates(k % 8,k / 8),image=self.pieces[self.piece_names.index(p)])
+                k += 1
+            else:
+                if p.isdigit():
+                    k += int(p)
+                continue
 
     def main(self):
 
         aboard = chess.Board()
+        aboard.push_san("e4")
+        aboard.push_san("e5")
+
+        # Initialize pieces
 
         master = Tkinter.Tk()
         frame = Tkinter.Frame(master, width=self.w, height=self.h,bd=self.border, relief=Tkinter.RAISED)
         frame.pack()
+
+        for color in ["black","white"]:
+            for name in self.piece_image_labels:
+                img = PIL.Image.open("./img/"+color+name+".png")
+                self.pieces.append(ImageTk.PhotoImage(img))
 
         self.myCanvas = Tkinter.Canvas(frame, width=self.w,height=self.h)
         self.myCanvas.pack()
@@ -54,6 +71,7 @@ class Core():
         # set the dimensions of the screen
         # and where it is placed
         master.geometry('%dx%d+%d+%d' % (self.w, self.h, x, y))
+
 
         master.mainloop()
 
